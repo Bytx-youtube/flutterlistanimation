b34280a32b54cff056d6d49226235a4687ccfd61
Bytx-youtube <80635487+Bytx-youtube@users.noreply.github.com>
2022-11-23 13:22:17 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MainScreen(),
+      home: const ListScreen(),
     );
   }
 }
